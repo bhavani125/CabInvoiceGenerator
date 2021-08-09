9b8e2e110fb75903887b57d2cedab7d0422ceccd
bhavani125 <girinenibhavani12@gmil.com>
2021-08-09 19:28:09 +0530

    Added:Code of calculating total fare

--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -1,6 +1,26 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class InvoiceServiceTest {
+    @Test
+    public void whenGivenDistanceAndTimeShouldReturnTotalFare() {
+        //Creating object for InvoiceGenerator class
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
+    }
 
-
+    @Test
+    public void WhenGivenLessDistanceAndTimeShouldReturnMiniFare() {
+        //Creating object for InvoiceGenerator class
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare,0.0);
+    }
 }
